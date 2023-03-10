5cda396f0ff977cdf4226fa4fed403723a53e250
sagar.jaiswal@jumio.com <sagar.jaiswal@jumio.com>
2023-03-10 18:15:04 +0530

    code

--- app.py
+++ app.py
@@ -1,26 +1,32 @@
 from transformers import pipeline
 import torch
+import onnxruntime
+from torchvision import transforms
+
 
 # Init is ran on server startup
 # Load your model to GPU as a global variable here using the variable name "model"
 def init():
     global model
-    
     device = 0 if torch.cuda.is_available() else -1
-    model = pipeline('fill-mask', model='bert-base-uncased', device=device)
 
-# Inference is ran for every server call
-# Reference your preloaded global model variable here.
-def inference(model_inputs:dict) -> dict:
+    model = onnxruntime.InferenceSession("mtailor.onnx", device=device)
+
+
+def to_numpy(self, tensor):
+    return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
+
+
+def inference(model_inputs: dict) -> dict:
     global model
 
     # Parse out your arguments
     prompt = model_inputs.get('prompt', None)
     if prompt == None:
         return {'message': "No prompt provided"}
-    
-    # Run the model
-    result = model(prompt)
 
-    # Return the results as a dictionary
-    return result
+    ort_inputs = {model.get_inputs()[0].name: to_numpy(prompt)}
+    model_output = model.run(None, ort_inputs)[0]
+    to_tensor = transforms.ToTensor()
+    onnx_res = torch.argmax(to_tensor(model_output).unsqueeze_(0))
+    return onnx_res
